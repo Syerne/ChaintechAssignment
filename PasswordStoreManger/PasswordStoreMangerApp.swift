6b6fbb721074878515a5cab8abc072dc868feed3
shubham yerne <shubham.vijay@myn.global>
2024-08-04 15:28:53 +0530

    project file added

---
+++ PasswordStoreManger/PasswordStoreMangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordStoreMangerApp.swift
+//  PasswordStoreManger
+//
+//  Created by shubam on 04/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PasswordStoreMangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
